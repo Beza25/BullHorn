2b3c9f0c392eae75ebadf9d14037be3a94ee98e0
GBTC405016UR <GBTC405016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-23 10:35:45 -0400

    BullHorn

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String content;
+    private String postedDate;
+    private String sentBy;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getPostedDate() {
+        return postedDate;
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setPostedDate(String postedDate) {
+        this.postedDate = postedDate;
+    }
+
+    public void setSentBy(String sentby) {
+        this.sentBy = sentby;
+    }
+
+
+}
